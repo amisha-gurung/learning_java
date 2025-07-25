50897f759f3fe587638c6268610207e08fc98ccc
amisha-gurung <amishagurung38@gmail.com>
2025-07-25 23:19:17 +0545

    Changing variables in java

---
+++ day_2/ChangingVar.java
@@ -0,0 +1,10 @@
+public class ChangingVar{
+    public static void main(String[] args){
+        int apples = 5;
+        System.out.println("Apples: " + apples);
+
+        apples = 10; // Change the value
+        System.out.println("Now apples: " + apples);
+
+    }
+}
