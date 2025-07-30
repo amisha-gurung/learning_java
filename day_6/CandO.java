ed1dc9716990ddc98e8b5fd0c412ac1f0e2c9ac6
amisha-gurung <amishagurung38@gmail.com>
2025-07-30 08:54:23 +0545

    Classes and Objects

---
+++ day_6/CandO.java
@@ -0,0 +1,9 @@
+public class CandO{
+    String name;
+    int age;
+    double height;
+
+    void Yo(){
+        System.out.println("Hieeee my name is " + name + " and I'm " + age + " years old. I'm " + height + "ft tall.");
+    }
+}
---
+++ day_6/Object.java
@@ -0,0 +1,9 @@
+public class Object{
+    public static void main(String[] args){
+        CandO coco = new CandO();
+        coco.name = "Ann";
+        coco.height = 5.5;
+        coco.age = 20;
+        coco.Yo();
+    }
+}
